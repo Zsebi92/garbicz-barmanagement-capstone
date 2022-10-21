22a1c0a9056d1a1416ce22a98a0871ec7f3cc121
Zsebi92 <59623854+Zsebi92@users.noreply.github.com>
2022-10-21 11:51:57 +0200

    Add Hello World fullstack fixed Test

--- backend/src/test/java/com/example/backend/contoller/HelloWorldControllerTest.java
+++ backend/src/test/java/com/example/backend/contoller/HelloWorldControllerTest.java
@@ -1,15 +1,16 @@
 package com.example.backend.contoller;
 
 import org.junit.jupiter.api.Test;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
+@SpringBootTest
 @AutoConfigureMockMvc
 class HelloWorldControllerTest {
 
@@ -18,14 +19,13 @@ class HelloWorldControllerTest {
 
     @Test
     void sayHello_shouldReturnHello() throws Exception {
-        // GIVEN
+        // Given
 
         // WHEN & THEN
         mockMvc.perform(
-                        MockMvcRequestBuilders.get("/api/hello"))
-                .andExpect(status().is(200))
+                MockMvcRequestBuilders.get("/api/hello"))
+                        .andExpect(status().is(200))
                 .andExpect(content().string("Hello World!"));
-
     }
 
 }
