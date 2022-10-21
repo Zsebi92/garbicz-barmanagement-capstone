83466922d6f57c7474a812603ea6fdbf8e501341
Zsebi92 <59623854+Zsebi92@users.noreply.github.com>
2022-10-21 12:58:29 +0200

    Setup Heroku deployment

--- backend/src/test/java/com/example/backend/contoller/HelloWorldControllerTest.java
+++ backend/src/test/java/com/example/backend/contoller/HelloWorldControllerTest.java
@@ -7,7 +7,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-import static org.junit.jupiter.api.Assertions.*;
+
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 @SpringBootTest
