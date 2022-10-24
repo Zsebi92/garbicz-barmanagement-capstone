49b0f47aebeb9dddfc543f6b7d0056ee5cc6e6f6
Zsebi92 <59623854+Zsebi92@users.noreply.github.com>
2022-10-24 08:52:41 +0200

    Inventory Model changed name and added  Strings Inventory Repo added

---
+++ backend/src/main/java/com/example/backend/model/Inventory_Model.java
@@ -0,0 +1,31 @@
+package com.example.backend.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Document("inventory")
+public class Inventory_Model {
+
+    /*  ekb = Einkaufsbrutto
+        ekn = Eikaufsnetto
+        fpk = Flaschen pro Kiste
+     */
+    @Id
+    String id;
+    String name;
+
+    String liter;
+    String ekb;
+    String ekn;
+    String fpk;
+    String kisten;
+    String paletten;
+}
