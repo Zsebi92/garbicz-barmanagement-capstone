2696bf6f6780cbfd3bcd0a4809964c6a0319df9b
Zsebi92 <59623854+Zsebi92@users.noreply.github.com>
2022-10-26 08:01:54 +0200

    FE: Added name for listing and size of bottle

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -13,7 +13,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>TEST</h1>
+        <h1>Add Listing</h1>
 
 
           <ListingGallery listings={listings}/>
--- frontend/src/components/AddListing.tsx
+++ frontend/src/components/AddListing.tsx
@@ -13,14 +13,31 @@ export default function AddListing(props: AddListingProps) {
     const nameOnChange = (event: ChangeEvent<HTMLInputElement>) => {
         setName(event.target.value)
     }
-    const literOnChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const literOnChange = (event: ChangeEvent<HTMLSelectElement>) => {
         setLiter(event.target.value)
     }
 
     return(
-        <div className={"search-bar"}>
-            <input onChange={nameOnChange} value={name}/>
-            <input onChange={literOnChange} value={liter}/>
+        <div className={"input-field"}>
+
+
+            <div className="input-group mb-3">
+                <span className="input-group-text" id="inputGroup-sizing-default">Name </span>
+                <input onChange={nameOnChange} value={name} type="text" className="form-control" aria-label="Sizing example input"
+                       aria-describedby="inputGroup-sizing-default"/>
+            </div>
+
+            <select onChange={literOnChange} value={liter} className="form-select" aria-label="Default select example">
+                <option selected>Flaschen Inhalt in Liter</option>
+                <option value="0,2l">0,2l</option>
+                <option value="0,33l">0,33l</option>
+                <option value="0,5l">0,5l</option>
+                <option value="0,75l">0,75l</option>
+                <option value="1,0l">1,0l</option>
+                <option value="1,5l">1,5l</option>
+            </select>
+
+
 
             <button onClick={() => props.addListing(name, liter)}>Add</button>
         </div>
